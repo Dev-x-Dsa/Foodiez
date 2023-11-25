53b79634cf7f69fe24b3e7fba8de3d09ae6ef031
Sukhpreet singh <101046716+Sukhpreet7137@users.noreply.github.com>
2023-11-25 19:53:33 +0000

    Current location access added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import {MenuContext} from "./ContextAPI/MenuContext";
 import { Outlet } from 'react-router';
+import useGeolocation from 'react-hook-geolocation';
 
 function App() {
 
   const {restaurant_data,setrestaurant_data,fetchdata}=useContext(MenuContext);
+  const [latitude, setlatitude] = useState(null);
+  const [longitude, setlongitude] = useState(null);
+  const [head,sethead]=useState(null);
+   
+  const geolocation = useGeolocation();
+
   useEffect(()=>{
-    fetchdata();
-  },[]);
+    if(!geolocation.error){
+      setlatitude(geolocation.latitude)
+      setlongitude(geolocation.longitude)
+    }
+  },[geolocation]);
+  
+  useEffect(()=>{
+    fetchdata(latitude,longitude);
+  },[latitude,longitude]);
 
 
   return (
--- src/ContextAPI/MenuContext.js
+++ src/ContextAPI/MenuContext.js
@@ -4,32 +4,34 @@ import { useParams } from "react-router";
 
 export const MenuContext=createContext();
 
-let restaurant_listurl="https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.971599&lng=77.594566&page_type=DESKTOP_WEB_LISTING";
 export default function MenuContextProvider({children}){
     const [restaurant_data,setrestaurant_data]=useState(null);
     const [resid,setresid]=useState(null);
     const [restaurant_menu,setrestaurant_menu]=useState(null);
     const [restaurant_info,setrestaurant_info]=useState(null);
     const [restaurant_info2,setrestaurant_info2]=useState(null);
-
-
-    let restaurant_menuurl=`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.971599&lng=77.594566&restaurantId=${resid}&submitAction=ENTER`;
-
-    async function fetchdata(){
+    const [lati,setlati]=useState(null);
+    const [longi,setlongi]=useState(null);
+    
+    
+    async function fetchdata(latitude,longitude){
+        setlati(latitude);
+        setlongi(longitude);
+        let restaurant_listurl=`https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=${latitude}&lng=${longitude}&page_type=DESKTOP_WEB_LISTING`;
         const output=await fetch(restaurant_listurl);
         const data=await output.json();
-
+        
         const restaurant_info=data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
         data?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-
+        
         setrestaurant_data(restaurant_info);
         
     }
-
+    
     async function fetchdata2(){
+        let restaurant_menuurl=`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lati}&lng=${longi}&restaurantId=${resid}&submitAction=ENTER`;
         const output=await fetch(restaurant_menuurl);
-        const data2 = await output.json();
-        console.log(data2)
+        const data2=await output.json();
         setrestaurant_info(data2?.data?.cards[0]?.card?.card?.info);
         setrestaurant_info2(data2);
     }
@@ -38,9 +40,6 @@ export default function MenuContextProvider({children}){
             fetchdata2();
     },[resid]);
 
-    useEffect(()=>{
-        console.log(restaurant_info2);
-    },[restaurant_info2]);
 
     const values={
         restaurant_data,
