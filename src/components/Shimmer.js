40dfa7411c9aa24548fcf272bc5ac057a1c150f4
Shivansh Singh <77519009+shivanshsinghx365@users.noreply.github.com>
2023-12-08 19:38:07 +0530

    Improved Shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,150 +2,168 @@ import React from 'react'
 const Shimmer = () => {
   return (
     <div className='flex flex-wrap justify-center items-center gap-x-14 gap-y-10 bg-slate'>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
-      <div class=" flex space-x-4 animate-pulse ">
-        <div className='w-[400px] rounded-3xl bg-gradient-to-r from-slate-600 to-slate-400 h-[250px]'>
+
+      <div class=" flex flex-col animate-pulse">
+        <div className='w-[400px] rounded-3xl bg-gray-300 h-[250px] '>
           <div className='w-full'>
-            <div className='w-full h-full object-cover rounded-3xl'></div>
-          </div>
-          <div>
           </div>
         </div>
+        <p class="mt-4 mb-1 w-full h-4 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-2/3 h-3 animate-pulse bg-gray-300 rounded-md"></p>
+        <p class="mt-3 mb-1 w-1/2 h-3 animate-pulse bg-gray-300 rounded-md"></p>
       </div>
+
+
     </div>
   )
 }
