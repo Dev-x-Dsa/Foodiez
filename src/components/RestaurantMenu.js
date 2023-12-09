6b8e6c08c965554c74cc5543259c9dd6f7ad9628
Shivansh Singh <77519009+shivanshsinghx365@users.noreply.github.com>
2023-12-09 11:58:03 +0530

    font-col

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -15,21 +15,21 @@ const RestaurantMenu = () => {
        restaurant_info2!==null && restaurant_info2!==undefined?
        (
             <div className='w-full min-h-screen h-full dark:bg-[#0d1117] py-10 dark:text-white font-Open'>
-            <div className='w-[800px] mx-auto'>
-            <p className=' font-semibold text-3xl capitalize w-full text-zinc-700 dark:text-slate-300'>{restaurant_info?.name}</p>
-           <p className='flex flex-row gap-x-2'>{
-            cusine.map((cusin)=>{
-                return <p>{cusin}</p>
-            })
-           }</p>
-            <p className="text-neutral-600 text-2xl font-thin">{restaurant_info?.areaName}</p>
-           {
-                restaurant_info2?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map((rest)=>{
-                    return <RestaurantMenuOpt key={id} rest={rest}/>
+                <div className='w-[800px] mx-auto'>
+                <p className=' font-semibold text-3xl capitalize w-full text-zinc-700 dark:text-slate-300'>{restaurant_info?.name}</p>
+            <p className='flex flex-row gap-x-2'>{
+                cusine.map((cusin)=>{
+                    return <p>{cusin}</p>
                 })
-            }
+            }</p>
+                <p className="text-neutral-600 dark:text-neutral-400 text-2xl font-thin">{restaurant_info?.areaName}</p>
+            {
+                    restaurant_info2?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.map((rest)=>{
+                        return <RestaurantMenuOpt key={id} rest={rest}/>
+                    })
+                }
+                </div>
             </div>
-        </div>
        ):
        <p>false</p>
   )
