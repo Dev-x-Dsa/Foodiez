d867fa61784ac0581b905f3e287de0cc5d2d6387
Shivansh Singh <77519009+shivanshsinghx365@users.noreply.github.com>
2023-12-25 20:01:26 +0530

    Hero Spinner Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import plate from "../images/plate.png"
+import best from "../images/hero.png"
 import { LocationContext } from "../ContextAPI/LocationContext";
 import { MenuContext } from "../ContextAPI/MenuContext";
+import './hero.css'
 
 const Hero = () => {
     const [locationdata,setlocationdata]=useState("Ludhiana");
@@ -25,10 +27,22 @@ const Hero = () => {
                 <input type="text" className="rounded-md text-black outline-none px-2 py-1 text-lg" onChange={(e)=>{setlocationdata(e.target.value)}} onKeyDown={(e)=>{if(e.key==="Enter"){getlatlong(locationdata)}}} />
             </div>
             <div className="relative w-[400px]">
-                <img src={plate} alt="" className="absolute -top-32 -right-32 w-full" />
+                {/* <img src={plate} alt="" className="absolute -top-32 -right-32 w-full" /> */}
+                <img src={best} alt="" className="absolute w-[120%] -top-52 -right-60 spinner"/>
             </div>
         </div>
     </div>)
 }
 
 export default Hero;
+
+/*
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+.animate-spin {
+    animation: spin 1s linear infinite;
+}
+ */
