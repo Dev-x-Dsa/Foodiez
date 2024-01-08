8fbdf4ec6aa0706d4432160d009cc9e4aad5b0eb
shivansh <77519009+shivanshsinghx365@users.noreply.github.com>
2024-01-08 12:24:23 +0530

    improved contact, color and fixed filters

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -85,25 +85,25 @@ const Body = () => {
                 </div>
 
                 <div className='flex gap-x-4 font-semibold'>
-                  <button onClick={() => {setb1(!b1); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b1?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b1?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => {setb1(!b1); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b1?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b1?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Top Restaurants
                   </button>
-                  <button onClick={() => {setb2(!b2); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b2?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b2?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => {setb2(!b2); filter30(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b2?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b2?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Under 30 Min
                   </button>
-                  <button onClick={() => {setb3(!b3); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b3?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b3?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => {setb3(!b3); filterVeg(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b3?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b3?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Veg Only
                   </button>
-                  <button onClick={() => {setb4(!b4); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b4?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b4?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => {setb4(!b4); filterdataonratings(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b4?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b4?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Rating 4.0+
                   </button>
-                  <button onClick={() => {setb5(!b5); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b5?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b5?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => {setb5(!b5); filterAb3(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b5?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b5?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Rating 3.0+
                   </button>
-                  <button onClick={() => {setb6(!b6); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b6?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b6?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => { setb6(!b6); filterAb2(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b6?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b6?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Rating 2.0+
                   </button>
-                  <button onClick={() => {setb7(!b7); filterBst(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b7?"bg-slate-100":"bg-red-700"}  text-slate-900 ${b7?"dark:bg-red-700":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
+                  <button onClick={() => {setb7(!b7); filterAb1(); }} className={`mb-5  ml-4 border py-1 px-2 dark:text-slate-200 dark:border-slate-800 ${b7?"bg-slate-100":"bg-green-600"}  text-slate-900 ${b7?"dark:bg-green-600":"dark:bg-[#24292f]"} rounded-lg text-xl`}>
                     Rating 1.0+
                   </button>
                 </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,16 @@
 import img from "../images/loadcrop.gif"
 import img1 from "../images/first.png";
 import img2 from "../images/last.png";
+import img3 from "../images/r_aside.jpg";
 import { toast } from "react-toastify";
 
 const About = () => {
-
-
     function feedbacksub(){
         toast.success("Feedback Submitted !!",{
             position: toast.POSITION.TOP_CENTER,
         })
     }
-    return <div className="dark:bg-[#0d1117] dark:text-slate-200 flex flex-col">
+    return <div className="dark:bg-[#0c111d] dark:text-slate-200 flex flex-col pt-6 overflow-clip">
         
             <div className="hidden dark:block w-screen">
                 <img src={img2} className="w-full"/>
@@ -20,40 +19,40 @@ const About = () => {
                 <img src={img1} className="w-full"/>
             </div>
 
-            <div className="flex flex-row w-10/12 mx-auto">
-                <div className="flex flex-col w-1/2 px-16 text-black gap-y-5">
-                    <p className=" text-3xl font-bold text-black dark:text-white">Lets level up your brand, together</p>
-                    <p>You can reach us anytime</p>
+            <div className="flex flex-row justify-between w-full">
+                <div className="flex flex-col mx-auto mt-10 text-black gap-y-2.5">
+                <p className=" text-3xl font-bold text-black dark:text-[#e5d8fa]">Lets level up your brand, together</p>
+                <p className="dark:text-[#f1f0f6]">You can reach us anytime</p>
                     <div className="flex flex-row gap-x-5">
                         <div className="w-full">
-                            <p className="text-black dark:text-white">First Name</p>
+                        <p className="text-black dark:text-[#f1f0f6]">First Name</p>
                             <input type="text" className="outline-none border border-black w-full rounded-lg px-2 py-1 text-xl"/>
                         </div>
                         <div className="w-full">
-                            <p className="text-black dark:text-white">Last Name</p>
+                        <p className="text-black dark:text-[#f1f0f6]">Last Name</p>
                             <input type="text" className="outline-none border border-black w-full rounded-lg px-2 py-1 text-xl"/>
                         </div>
                     </div>
                     <div className="flex flex-col">
-                        <p className="text-black dark:text-white">Email</p>
+                    <p className="text-black dark:text-[#f1f0f6]">Email</p>
                         <input type="email" className="outline-none border border-black rounded-lg px-2 py-1 text-xl"/>
                     </div>
                     <div className="flex flex-col">
-                        <p className="text-black dark:text-white">Phone number</p>
+                    <p className="text-black dark:text-[#f1f0f6]">Phone number</p>
                         <input type="text" className="outline-none border border-black rounded-lg px-2 py-1 text-xl"/>
                     </div>
                     <div className="flex flex-col">
-                            <p className="text-black dark:text-white">Message</p>
+                    <p className="text-black dark:text-[#f1f0f6]">Message</p>
                             <textarea placeholder="Leave us a message" className="outline-none h-48 border border-black rows={4} cols={50} rounded-lg px-2 py-1 text-xl"/>
                     </div>
                     <div className="w-full py-4">
-                        <button onClick={()=>{feedbacksub()}} className="w-full bg-yellow-600 py-1 px-2 rounded-lg text-2xl">
+                    <button onClick={() => { feedbacksub() }} className="w-full bg-[#a17bec] py-1 px-2 rounded-lg text-2xl">
                             Submit
                         </button>
                     </div>
                 </div>
-                <div className="w-1/2">
-                    <img src={img2}/>
+                <div className="overflow-clip">
+                    <img src={img3}/>
                 </div>
             </div>
         </div>
