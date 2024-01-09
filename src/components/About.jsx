11f084e3d76f365493e5909dfb8a0ace25b3948c
Sukhpreet singh <101046716+Sukhpreet7137@users.noreply.github.com>
2024-01-09 08:17:44 +0000

    about added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,40 @@
-import img from "../images/instagram.png"
+import img from "../images/download.jpg"
+
 const Contact = () => {
-    return <div className="py-12 dark:bg-[#0d1117] dark:text-slate-200 flex flex-col">
-        <h1 className="dark:text-slate-200 text-5xl"> Get in Touch </h1>
-        <br /><br />
-        <div className="flex">
-            <img src={img} alt="" className="m-5"/>
-            <div className="flex flex-col text-2xl w-[50rem] gap-2">
-                <h2 className="dark:text-slate-300 text-3xl">Phone No: 123456789 📞</h2>
-                <h2 className="dark:text-slate-300 text-3xl">Email   : Foodiez@email.com ✉</h2>
-                <br /><br />
-                <form action="">
-                    <label for="fname">First name : </label><input type="text" id="fname" name="fname" className="outline-none px-2 mx-2 my-2 "></input><br />
-                    <label for="lname">Last name : </label><input type="text" id="lname" name="lname" className="outline-none px-2 mx-2 my-2 "></input><br />
-                    <label for="cnt">Contact Number : </label><input type="phone" id="cnt" name="cnt" className="outline-none px-2 mx-2 my-2 "></input><br />
-                    <label for="eml">Email Id : </label><input type="mail" id="eml" name="eml" className="outline-none px-2 mx-2 my-2 "></input><br />
-                </form>
+    return <div className="dark:bg-[#0d1117] dark:text-white">
+            <div className="flex flex-col w-3/4 px-10 mx-auto py-16 gap-y-20">
+            <div className="flex flex-row gap-x-2">
+            <p className="min-w-[15%] text-2xl">Who we are</p>
+                <p className="text-2xl text-justify">
+                He ordered his regular breakfast. Two eggs sunnyside up, hash browns, and two strips of bacon. He continued to look at the menu wondering if this would be the day he added something new. This was also part of the routine. A few seconds of hesitation to see if something else would be added to the order before demuring and saying that would be all. It was the same exact meal that he had ordered every day for the past two years
+                He ordered his regular breakfast. Two eggs sunnyside up, hash browns, and two strips of bacon. He continued to look at the menu wondering if this would be the day he added something new. This was also part of the routine. A few seconds of hesitation to see if something else would be added to the order before demuring and saying that would be all. It was the same exact meal that he had ordered every day for the past two years
+                </p>
+            </div>
+            <div className="flex justify-between">
+                <div className="w-[30%] bg-[#0d1097] text-2xl bg-red-700 p-10 gap-y-6 flex flex-col">
+                   <h2 className="text-yellow-600">Leadership Principles</h2>
+                   <p>
+                        Our Leadership Principles are more than inspirational wall hangings. The 16 principles guide our discussions and decisions every day.
+                   </p>
+                   <button className="w-fit border border-grey-300 rounded-full py-3 px-3">Learn More➡️</button>
+                </div>
+                <div className="w-[30%] bg-[#0d1097] text-2xl bg-red-700 p-10 gap-y-6 flex flex-col">
+                    <h2 className="text-yellow-600">Leadership Principles</h2>
+                    <p>
+                            Our Leadership Principles are more than inspirational wall hangings. The 16 principles guide our discussions and decisions every day.
+                    </p>
+                    <button className="w-fit border border-grey-300 rounded-full py-3 px-3">Learn More➡️</button>
+                </div>
+                <div className="w-[30%] bg-[#0d1097] text-2xl bg-red-700 p-10 gap-y-6 flex flex-col">
+                    <h2 className="text-yellow-600"> Leadership Principles</h2>
+                    <p>
+                            Our Leadership Principles are more than inspirational wall hangings. The 16 principles guide our discussions and decisions every day.
+                    </p>
+                    <button className="w-fit border border-grey-300 rounded-full py-3 px-3">Learn More➡️</button>
+                </div>
+            </div>
+            <div className="w-full">
+                <img src={img} className="w-full"/>
             </div>
         </div>
     </div>
