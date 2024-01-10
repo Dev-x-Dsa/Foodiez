2938d1e5b7cff889db59648b2bc4f4aeb6561383
Shivansh Singh <77519009+shivanshsinghx365@users.noreply.github.com>
2024-01-10 18:37:58 +0530

    Improved image for menu

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -27,31 +27,38 @@ const RestaurantMenu = () => {
             (
                 <div className='w-full min-h-screen h-full dark:bg-[#0d1117] py-10 dark:text-white font-Open'>
                     <div className='w-[800px] mx-auto flex flex-col'>
-                        <p className=' font-semibold text-3xl capitalize w-full text-zinc-700 dark:text-slate-300'>{restaurant_info?.name}</p>
-                        <img className='h-48 w-72' src={CDN_URL + restaurant_info?.cloudinaryImageId} />
+                        <p className=' font-semibold text-3xl capitalize w-full text-zinc-700 dark:text-slate-100'>{restaurant_info?.name}</p>
+                        <div className="h-[250px] w-full rounded-lg overflow-clip my-5 relative before:absolute before:left-0 before:right-0 before:top-0 before:z-10 before:h-full before:w-full before:bg-gradient-to-t  from-black to-40% text-lg group text-white">
+                            <img className='overflow-hidden w-full absolute -top-3' src={CDN_URL + restaurant_info?.cloudinaryImageId} />
+                                <div className="absolute left-4 bottom-4 text-2xl text-slate-100 z-20">{restaurant_info?.areaName}</div>
+                            <div className="absolute right-4 bottom-4 text-2xl text-slate-100 z-20"><span className="text-slate-100 text-2xl font-thin flex items-center">
+                                <img src={clock} alt="" className='w-[1.5rem] h-[1.5rem] dark:invert mx-2' />
+                                <span className='lowercase'>{restaurant_info?.sla?.slaString}</span>
+                            </span></div>
+                        </div>
                         <p className='flex flex-row gap-x-2'>{
                             cusine.map((cusin) => {
                                 return <p>{cusin}</p>
                             })
                         }</p>
                         <div className="flex justify-between">
-                            <p className="text-neutral-600 dark:text-neutral-400 text-2xl font-thin">{restaurant_info?.areaName}</p>
+                            {/* <p className="text-neutral-600 dark:text-neutral-400 text-2xl font-thin">{restaurant_info?.areaName}</p>
                             <span className="text-neutral-600 dark:text-neutral-400 text-2xl font-thin flex items-center">
                                 <img src={clock} alt="" className='w-[1.5rem] h-[1.5rem] dark:invert mx-2' />
                                 <span className='lowercase'>{restaurant_info?.sla?.slaString}</span>
-                            </span>
-                            <div className='flex flex-row'>
-                                <p>Veg only</p>
+                            </span> */}
+                            <div className='flex flex-row items-center'>
+                                <p className='pr-5 text-slate-100 text-2xl font-thin'>Veg Only</p>
                                     {vegonly ?
                                         (
-                                            <button onClick={() => { setvegonly(prev => !prev) }} className="h-[25px] bg-white rounded-full border border-black w-[50px] flex flex-row">
+                                            <button onClick={() => { setvegonly(prev => !prev) }} className="h-[25px] bg-white rounded-full w-[50px] flex flex-row">
                                                 <div className='rounded-full bg-white min-h-full min-w-[50%]'></div>
-                                                <div className='rounded-full min-w-[50%] min-h-full bg-green-700'></div>
+                                                <div className='rounded-full min-w-[50%] min-h-full bg-green-500'></div>
                                             </button>
                                         ) :
                                         (
-                                            <button onClick={() => { setvegonly(prev => !prev) }} className="h-[25px] bg-white rounded-full border border-black w-[50px] flex flex-row">
-                                                <div className='rounded-full min-w-[50%] min-h-full bg-red-700'></div>
+                                            <button onClick={() => { setvegonly(prev => !prev) }} className="h-[25px] bg-white rounded-full w-[50px] flex flex-row">
+                                                <div className='rounded-full min-w-[50%] min-h-full bg-red-600'></div>
                                             </button>
                                         )
 
