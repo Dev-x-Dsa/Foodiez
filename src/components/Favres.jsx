89502ad5e85bbef17661fa4b8fb1d20b4cc820f1
Sukhpreet singh <101046716+Sukhpreet7137@users.noreply.github.com>
2024-01-21 17:50:32 +0000

    ..f

--- src/components/Favres.jsx
+++ src/components/Favres.jsx
@@ -12,23 +12,21 @@ const Favres = () => {
 
 
     var favres = useSelector(store => store.favres.items);
-    
-    if(!favres){
-        return <div className='py-20 flex flex-wrap justify-evenly items-center gap-y-12 font-Open dark:bg-[#0d1117]'>
-            <img src={imgg}/>
-        </div>
-    }
 
-    
-    
+
     useEffect(() => {
-        if (favres.length === 0 && localStorage.getItem("fav-items")) {
+        if (favres && favres!==undefined && favres.length === 0 && localStorage.getItem("fav-items")) {
             favres = JSON.parse(localStorage.getItem('fav-items'));
         }
         localStorage.setItem('isCartEmptied', 'false');
     }, [])
 
 
+    if(!favres){
+        return <div className='py-20 flex flex-wrap justify-evenly items-center gap-y-12 font-Open dark:bg-[#0d1117]'>
+            <img src={imgg}/>
+        </div>
+    }
 
     return (
         <div className='py-20 pt-36 flex flex-wrap justify-evenly items-center gap-y-12 font-Open dark:bg-[#0d1117]'>
